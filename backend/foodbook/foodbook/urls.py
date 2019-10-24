09f754bbcf17d5737a7fdf611089b602e693fbaf
ByungHeeCha <paulcha1025@snu.ac.kr>
2019-10-24 20:40:56 +0900

    ready for travis

--- backend/foodbook/foodbook/urls.py
+++ backend/foodbook/foodbook/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('foodbook_api.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/foodbook/foodbook_api/views.py
+++ backend/foodbook/foodbook_api/views.py
@@ -1,5 +1,3 @@
-
-
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Profile, Review
