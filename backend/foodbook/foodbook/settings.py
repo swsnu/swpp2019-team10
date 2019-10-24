1f14c8dd9cf4d095ebe34a51e3c31b2a99bac38d
ByungHeeCha <paulcha1025@snu.ac.kr>
2019-10-24 16:24:35 +0900

    add djangoapp foodbook_api

--- backend/foodbook/foodbook/settings.py
+++ backend/foodbook/foodbook/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'location_field.apps.DefaultConfig',
     'foodbook_api.apps.FoodbookApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
