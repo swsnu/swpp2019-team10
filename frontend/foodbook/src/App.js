cc57c4ff4bfb529fe1ef93e3ac1a2a9cb0aefaef
JaehoNam <njh215@snu.ac.kr>
2019-10-24 20:32:25 +0900

    Update App.js

--- frontend/foodbook/src/App.js
+++ frontend/foodbook/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          <code
+          <code>
             src/App.js
           </code>
           and save to reload.
