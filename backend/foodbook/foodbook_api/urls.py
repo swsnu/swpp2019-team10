a2a25d87c4465a6e6434a92ff5682596ca71238b
ByungHeeCha <paulcha1025@snu.ac.kr>
2019-10-25 14:30:10 +0900

    add views.review_list
    minor changes:
    add url for views.review_list
    set Response.friend to empty list when making new User

--- backend/foodbook/foodbook_api/urls.py
+++ backend/foodbook/foodbook_api/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.user, name='user'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('review/', views.review_list, name='review_list')
 ]
