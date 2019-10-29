faf606598e87dc6f221c4977900b6ea32231ab71
JaehoNam <njh215@snu.ac.kr>
2019-10-30 00:24:54 +0900

    add apis related to friend in user_view.py

--- backend/foodbook/foodbook_api/urls.py
+++ backend/foodbook/foodbook_api/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from .views import review_views, user_views
 
 urlpatterns = [
-    path('', views.user, name='user'),
-    path('signup/', views.signup, name='signup'),
-    path('signin/', views.signin, name='signin'),
-    path('signout/', views.signout, name='signout'),
-    path('review/', views.review_list, name='review_list'),
-    path('review/<int:review_id>/', views.review_detail, name='review_detail')
+    path('', user_views.user, name='user'),
+    path('signup/', user_views.signup, name='signup'),
+    path('signin/', user_views.signin, name='signin'),
+    path('signout/', user_views.signout, name='signout'),
+    path('friend/', user_views.friend, name='friend'),
+    path('friend/<int:friend_id>/', user_views.friend_detail, name='friend_detail'),
+    path('review/', review_views.review_list, name='review_list'),
+    path('review/<int:review_id>/', review_views.review_detail, name='review_detail'),
 ]
