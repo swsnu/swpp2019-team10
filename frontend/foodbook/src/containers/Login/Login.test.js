ddf30b1534b114b5904c01c60b15673ec3ba0677
jyeok <chez2s@snu.ac.kr>
2019-10-31 10:59:57 +0900

    eslint error solved

--- frontend/foodbook/src/containers/Login/Login.test.js
+++ frontend/foodbook/src/containers/Login/Login.test.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import Login from './Login';
-import { Provider } from 'react-redux';
-import { connectRouter, ConnectedRouter } from 'connected-react-router';
+import { shallow } from 'enzyme';
 import { history } from 'store/store';
+import Login from './Login';
 
 describe('<Login />', () => {
-    let login;
+  beforeEach(() => {
+  });
 
-    beforeEach(() => {
-    });
+  it('should render without crashing', () => {
+    const component = shallow(<Login history={history} />);
+    const wrapper = component.find('.login');
+    expect(wrapper.length).toBe(1);
+  });
 
-    it('should render without crashing', () => {
-        const component = shallow(<Login history={history} />);
-        const wrapper = component.find(".login");
-        expect(wrapper.length).toBe(1);
-    });
-
-    it('should call loginHandler function', () => {
-        const component = shallow(<Login history={history} />);
-        const wrapper = component.find('#login-button');
-        const spy = jest.spyOn(history, 'push')
-        .mockImplementation(() => {});
-        wrapper.simulate('click');
-        expect(spy).toHaveBeenCalledTimes(1);
-    });
+  it('should call loginHandler function', () => {
+    const component = shallow(<Login history={history} />);
+    const wrapper = component.find('#login-button');
+    const spy = jest.spyOn(history, 'push')
+      .mockImplementation(() => {});
+    wrapper.simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
 });
