0b0d9da483da707b5c208581e414c459e1eb5d14
laptop <54742258+th-snu@users.noreply.github.com>
2019-10-31 18:38:00 +0900

    image-select-pv depenency and indev

--- frontend/foodbook/src/components/AddReview/AddReview.js
+++ frontend/foodbook/src/components/AddReview/AddReview.js
@@ -4,11 +4,17 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 
 import { connect } from 'react-redux';
+<<<<<<< Updated upstream
+=======
+import { axios } from 'axios';
+import ImageSelectPreview from 'react-image-select-pv';
+>>>>>>> Stashed changes
 // import * as actionCreators from '../../Stores/Actions/index';
 
 class AddReview extends Component {
   componentDidMount() {
     this.setState({
+<<<<<<< Updated upstream
       // image: null,
       // content: '',
     });
@@ -20,6 +26,34 @@ class AddReview extends Component {
 
   render() {
     return <Button id="upldPicButton">UploadPicture</Button>;
+=======
+      restauerant: '',
+      menu: '',
+      content: '',
+      rating: null,
+      image: null,
+    });
+  }
+
+  addReviewHandler = () => {
+    const fd = new FormData();
+    const file = new File([this.image], 'random_name.jpg');
+    fd.append('image', file);
+    axios.post();
+  }
+
+  imageHandler = (data) => {
+    this.setState({image: data.content });
+  }
+
+  render() {
+    const imgUpload = <div><ImageSelectPreview onChange={(data) => this.imageHandler(data)} /></div>;
+    return (
+      <div className="addReview">
+        {imgUpload}
+      </div>
+    );
+>>>>>>> Stashed changes
   }
 }
 
