7c0c785e777d352796f4cf5793468f60677e736d
laptop <54742258+th-snu@users.noreply.github.com>
2019-10-31 18:55:29 +0900

    removed conflict message

--- frontend/foodbook/src/components/AddReview/AddReview.js
+++ frontend/foodbook/src/components/AddReview/AddReview.js
@@ -1,59 +1,50 @@
 /*  eslint linebreak-style: ["error", "windows"]  */
 
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+// import { Button } from 'semantic-ui-react';
 
 import { connect } from 'react-redux';
-<<<<<<< Updated upstream
-=======
 import { axios } from 'axios';
 import ImageSelectPreview from 'react-image-select-pv';
->>>>>>> Stashed changes
 // import * as actionCreators from '../../Stores/Actions/index';
 
 class AddReview extends Component {
   componentDidMount() {
     this.setState({
-<<<<<<< Updated upstream
-      // image: null,
+      // restauerant: '',
+      // menu: '',
       // content: '',
-    });
-  }
-
-  createArticleHandler = () => {
-
-  }
-
-  render() {
-    return <Button id="upldPicButton">UploadPicture</Button>;
-=======
-      restauerant: '',
-      menu: '',
-      content: '',
-      rating: null,
+      // rating: null,
       image: null,
     });
   }
 
   addReviewHandler = () => {
+    const { image } = this.state;
+
     const fd = new FormData();
-    const file = new File([this.image], 'random_name.jpg');
+    const file = new File([image], 'img.jpg');
+
     fd.append('image', file);
-    axios.post();
+    axios.post({ data: fd });
   }
 
   imageHandler = (data) => {
-    this.setState({image: data.content });
+    this.setState({ image: data.content });
   }
 
   render() {
-    const imgUpload = <div><ImageSelectPreview onChange={(data) => this.imageHandler(data)} /></div>;
+    const imgUpload = (
+      <div>
+        <ImageSelectPreview onChange={(data) => this.imageHandler(data)} />
+      </div>
+    );
+
     return (
       <div className="addReview">
         {imgUpload}
       </div>
     );
->>>>>>> Stashed changes
   }
 }
 
