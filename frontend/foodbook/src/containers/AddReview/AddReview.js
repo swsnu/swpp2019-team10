c16a2fdb1f31b9eb9088d4959f5632b64c311305
laptop <54742258+th-snu@users.noreply.github.com>
2019-11-04 12:59:10 +0900

    Redesigned Add Review

--- frontend/foodbook/src/containers/AddReview/AddReview.js
+++ frontend/foodbook/src/containers/AddReview/AddReview.js
@@ -1,29 +1,47 @@
 import {
-  Container,
-  Grid,
+  // Container,
+  // Grid,
+  Rating,
   // Header,
   // Menu,
 } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+import PropTypes from 'prop-types';
 
 import React, { Component } from 'react';
-// import { Button } from 'semantic-ui-react';
+import './AddReview.css';
 
 import { connect } from 'react-redux';
 import { axios } from 'axios';
 import ImageSelectPreview from 'react-image-select-pv';
-import StarRating from '../../components/StarRating/StarRating';
+
+// import for tag handling function, it'd be better to make tag js separately
+import ReivewPreview from '../../components/ReviewPreview';
+
+// import StarRating from '../../components/StarRating/StarRating';
 // import * as actionCreators from '../../Stores/Actions/index';
 
 class AddReview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ready: false,
+    };
+  }
+
   componentDidMount() {
     this.setState({
-      // restauerant: '',
-      // menu: '',
-      // content: '',
+      restaurant: '',
+      menu: '',
+      content: '',
       rating: 0,
+      tag: [],
       // https://codepen.io/depy/pen/vEWWdw
       // location: null,
       image: null,
+      error: null,
+      ready: true,
     });
   }
 
@@ -39,10 +57,22 @@ class AddReview extends Component {
   }
 
   postContentHandler = () => {
-    const { rating } = this.state;
-    const reviewDict = {
+    const {
+      restaurant,
+      menu,
+      content,
       rating,
+      // tag,
+    } = this.state;
+
+    const reviewDict = {
+      restaurant_name: restaurant,
+      menu_name: menu,
+      content,
+      rating,
+      // tag,
     };
+
     axios.post('/api/review/', reviewDict);
 
     return 0;
@@ -69,28 +99,123 @@ class AddReview extends Component {
       </div>
     );
 
-    /* to be worked on */
-    const googleMap = (<div className="locationGoogle" />);
+    const {
+      rating, content, restaurant, menu, ready, error,
+    } = this.state;
+
+    const { history } = this.props;
+
+    if (!ready) {
+      if (error != null) {
+        history.push('/main');
+        return (
+          <div className="ReviewDetailError">
+            <p>{error}</p>
+          </div>
+        );
+      }
+      return (
+        <div className="ReviewDetailLoading">
+          <p>Loading...</p>
+        </div>
+      );
+    }
+
+    const googleMap = (<div className="locationGoogle"> Map will be here </div>);
+
+    const tagArea = <div />;
+    /*
+    (
+      <div className="tags">
+        <span className="tagShow">{ReivewPreview.parseTagName(tag)}</span>
+      </div>
+    );
+    */
+
+    const confirmDisabled = false;
 
     return (
       <div className="addReview">
-        <Container text>
-          <Grid rows={2} divided>
-            <Grid.Row className="upload">
-              <Grid.Column width={10}>
-                {imgUpload}
-              </Grid.Column>
-              <Grid.Column width={10}>
-                {googleMap}
-              </Grid.Column>
-            </Grid.Row>
-            <StarRating onChange={(rating) => this.setState({ rating })} />
-          </Grid>
-        </Container>
+        <div className="ui special cards">
+          <div className="card" style={{ width: '630px' }}>
+            <div className="content">
+              <span className="header">
+                Author name will be here
+              </span>
+              <div className="meta">
+                <span className="rating">
+                  Rating:
+                  <Rating defaultRating={rating} maxRating="5" icon="star" />
+                </span>
+                {tagArea}
+              </div>
+            </div>
+            <div className="blurring dimmable image">
+              {imgUpload}
+            </div>
+            <div className="google map">
+              {googleMap}
+            </div>
+            Restaurant
+            <textarea
+              id="review-restaurant-input"
+              rows="1"
+              type="text"
+              value={restaurant}
+              onChange={(event) => this.setState({ restaurant: event.target.value })}
+            />
+            <br />
+            Menu
+            <textarea
+              id="review-menu-input"
+              rows="1"
+              type="text"
+              value={menu}
+              onChange={(event) => this.setState({ menu: event.target.value })}
+            />
+            <br />
+            Content
+            <textarea
+              id="review-content-input"
+              rows="4"
+              type="text"
+              value={content}
+              onChange={(event) => this.setState({ content: event.target.value })}
+            />
+            <br />
+            <button
+              id="back-add-review-button"
+              type="button"
+              onClick={() => history.push('/articles')}
+            >
+              Back
+            </button>
+            <button
+              id="submit-review-button"
+              type="button"
+              disabled={confirmDisabled}
+              onClick={() => { this.addReviewHandler(); }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+AddReview.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+};
+
+AddReview.defaultProps = {
+  history: {
+    push: () => {},
+  },
+};
 
 const mapDispatchToProps = (dispatch) => ({
   dispatch,
