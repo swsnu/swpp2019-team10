4688555dbfb49debec246266c1cf9f9d61438119
jyeok <chez2s@snu.ac.kr>
2019-11-05 08:14:54 +0900

    ReviewList and ReviewPreview modified

--- frontend/foodbook/src/containers/ReviewList/ReviewList.js
+++ frontend/foodbook/src/containers/ReviewList/ReviewList.js
@@ -9,41 +9,61 @@ import * as actionCreators from 'store/actions/review/action_review';
 const ReviewList = (props) => {
   const { reviews, dateString } = props;
 
-  let reviewsToRender = reviews ? reviews : [];
-  if(dateString) reviewsToRender = reviewsToRender.filter(review => review.date === dateString);
+  let reviewsToRender = reviews || [];
+  if (dateString) reviewsToRender = reviewsToRender.filter((review) => review.date === dateString);
 
-  reviewsToRender = reviewsToRender.map(review => {
-    return (<ReviewPreview key={review.id} {...review} />);
-  });
+  reviewsToRender = reviewsToRender.map((review) => (
+    <ReviewPreview
+      key={review.id}
+      id={-1}
+      author="React"
+      restaurant="302"
+      menu="GalbiTang"
+      content="ZMTGR"
+      image="http://recipe1.ezmember.co.kr/cache/recipe/2017/11/15/268859d03a5850821af32b009d8001731.jpg"
+      rating={3}
+      date="2019-11-04"
+      tag={[{ name: 'hot', positive: true }, { name: 'pricy', positive: false }]}
+      isMine
+    />
+  ));
 
   return (
-  <div className="ReviewList">
-    <div className="ui special cards fluid">
-      <div className="card fluid" style={{ width: '630px' }}>
-        <div className="content">
-          <br />
-          {reviewsToRender}
-          <ReviewPreview
-            key={-1}
-            id={-1}
-            author={React}
-            restaurant= {'302'}
-            menu="GalbiTang"
-            content="ZMTGR"
-            image="http://recipe1.ezmember.co.kr/cache/recipe/2017/11/15/268859d03a5850821af32b009d8001731.jpg"
-            rating={3}
-            date="2019-11-04"
-            tag={[{ name: 'hot', positive: true }, { name: 'pricy', positive: false }]}
-          />
+    <div className="ReviewList">
+      <div className="ui special cards fluid">
+        <div className="card fluid" style={{ width: '630px' }}>
+          <div className="content">
+            <br />
+            {reviewsToRender}
+            <ReviewPreview
+              key="-1"
+              id={-1}
+              author="React"
+              restaurant="302"
+              menu="GalbiTang"
+              content="ZMTGR"
+              image="http://recipe1.ezmember.co.kr/cache/recipe/2017/11/15/268859d03a5850821af32b009d8001731.jpg"
+              rating={3}
+              date="2019-11-04"
+              tag={[{ name: 'hot', positive: true }, { name: 'pricy', positive: false }]}
+              isMine
+            />
+            {/* mock */}
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
-}
+};
 
 ReviewList.propTypes = {
   dateString: propTypes.string,
+  reviews: propTypes.arrayOf(Object),
+};
+
+ReviewList.defaultProps = {
+  dateString: undefined,
+  reviews: undefined,
 };
 
 const mapStateToProps = (state) => ({
