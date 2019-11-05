e7490069ecda59613404c152b4c52766ccbbe797
jyeok <chez2s@snu.ac.kr>
2019-11-05 19:44:12 +0900

    Test for Main

--- frontend/foodbook/src/components/Main.test.js
+++ frontend/foodbook/src/components/Main.test.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import { history } from 'store/store';
 import { ConnectedRouter } from 'connected-react-router';
 import { getMockStore } from 'test-utils/mock';
 import { Provider } from 'react-redux';
 import Main from './Main';
+// import ReviewList from 'containers/ReviewList/ReviewList';
 // import * as actionCreators from 'store/actions/user/action_user';
 
 const mockStore = getMockStore({}, {}, {});
 
+jest.mock('containers/ReviewList/ReviewList', () => jest.fn(() => (
+  <div className="mockReviewList">
+            this is mock
+  </div>
+)));
+
+jest.mock('containers/FriendList/FriendList', () => jest.fn(() => (
+  <div className="mockFriendList">
+            this is mock
+  </div>
+)));
+
+jest.mock('components/RawCalendar/RawCalendar', () => jest.fn(() => (
+  <div className="mockRawCalendar">
+            this is mock
+  </div>
+)));
+
 describe('main', () => {
   let main;
+
   beforeEach(() => {
     main = (
       <Provider store={mockStore}>
