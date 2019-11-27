f47391a1070d59620d0a9b006751d01d1d2568d7
jyeok <chez2s@snu.ac.kr>
2019-11-27 11:25:43 +0900

    syncing

--- frontend/foodbook/src/components/Main.js
+++ frontend/foodbook/src/components/Main.js
@@ -74,7 +74,7 @@ export default class Main extends Component {
     return (
       <div className="main">
         <Container text style={{ marginTop: '2em' }}>
-          <Header as="h1"><NavLink to="/">FoodBook</NavLink></Header>
+          <Header as="h1"><NavLink to="/aa">FoodBook</NavLink></Header>
           <Logout />
         </Container>
         {/* Title Region */}
