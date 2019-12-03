bb6903516e64647532317baf8f3b2faf52a4d96f
jyeok <chez2s@snu.ac.kr>
2019-12-04 03:24:44 +0900

    Logout test write

--- frontend/foodbook/src/containers/Logout/Logout.test.js
+++ frontend/foodbook/src/containers/Logout/Logout.test.js
@@ -1,100 +1,103 @@
-// import React from 'react';
-// import { mount } from 'enzyme';
-// import { history } from 'store/store';
-// import { ConnectedRouter } from 'connected-react-router';
-// import { getMockStore } from 'test-utils/mock';
-// import { Provider } from 'react-redux';
+import React from 'react';
+import { mount } from 'enzyme';
+import { history } from 'store/store';
+import { ConnectedRouter } from 'connected-react-router';
+import { getMockStore } from 'test-utils/mock';
+import { Provider } from 'react-redux';
 
-// import * as actionCreators from 'store/actions/user/action_user';
-// import Logout from '.';
+import * as actionCreators from 'store/actions/user/action_user';
+import Logout from '.';
+import Axios from 'axios';
 
-// const loggedinUser = {
-//   user: {
-//     username: '',
-//     phone_number: '',
-//     age: -1,
-//     gender: '',
-//     profile_pic: '',
-//     number_of_reviews: -1,
-//     number_of_friends: -1,
-//     failed: false,
-//   },
+const loggedinUser = {
+  user: {
+    username: '',
+    phone_number: '',
+    age: -1,
+    gender: '',
+    number_of_reviews: -1,
+    number_of_friends: -1,
+    failed: false,
+  },
 
-//   taste: {},
+  taste: {},
 
-//   search: -1,
-//   logged_in: true,
-// };
+  search: -1,
+  logged_in: true,
+};
 
-// const loggedoutUser = {
-//   user: {
-//     username: '',
-//     phone_number: '',
-//     age: -1,
-//     gender: '',
-//     profile_pic: '',
-//     number_of_reviews: -1,
-//     number_of_friends: -1,
-//     failed: false,
-//   },
+const loggedoutUser = {
+  user: {
+    username: '',
+    phone_number: '',
+    age: -1,
+    gender: '',
+    number_of_reviews: -1,
+    number_of_friends: -1,
+    failed: false,
+  },
 
-//   taste: {},
+  taste: {},
 
-//   search: -1,
-//   logged_in: false,
-// };
+  search: -1,
+  logged_in: false,
+};
 
-// const store = getMockStore(loggedinUser, {}, {});
-// const loggedOutStore = getMockStore(loggedoutUser, {}, {});
+const store = getMockStore(loggedinUser, {}, {});
+const loggedOutStore = getMockStore(loggedoutUser, {}, {});
 
-// describe('<Logout />', () => {
-//   let logout;
-//   const spyLogout = jest.spyOn(actionCreators, 'LOGOUT')
-//     .mockImplementation(() => ({ type: '' }));
+describe('<Logout />', () => {
+  let logout;
+  const spyLogout = jest.spyOn(Axios, 'get')
+    .mockImplementation(() => {
+        return new Promise((res) => {
+            res();
+        })
+    });
 
-//   beforeEach(() => {
-//     logout = (
-//       <Provider store={store}>
-//         <ConnectedRouter history={history}>
-//           <Logout />
-//         </ConnectedRouter>
-//       </Provider>
-//     );
-//   });
+  beforeEach(() => {
+    logout = (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Logout />
+        </ConnectedRouter>
+      </Provider>
+    );
+  });
 
-//   afterEach(() => {
-//     jest.clearAllMocks();
-//   });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-//   it('should render without crashing', () => {
-//     const component = mount(logout);
-//     const wrapper = component.find('.logout');
-//     expect(wrapper.length).toBe(1);
-//   });
+  it('should render without crashing', () => {
+    const component = mount(logout);
+    const wrapper = component.find('.logout');
+    expect(wrapper.length).toBe(1);
+  });
 
-//   it('should dispatch logout action', () => {
-//     const component = mount(logout);
-//     const wrapper = component.find('Button').at(0);
+  it('should dispatch logout action', () => {
+    const component = mount(logout);
+    const wrapper = component.find('Button').at(0);
 
-//     wrapper.simulate('click');
+    wrapper.simulate('click');
 
-//     expect(spyLogout).toHaveBeenCalledTimes(1);
-//   });
+    expect(spyLogout).toHaveBeenCalledTimes(1);
+  });
 
-//   it('should not crash when not logged in', () => {
-//     const falseLogout = (
-//       <Provider store={loggedOutStore}>
-//         <ConnectedRouter history={history}>
-//           <Logout />
-//         </ConnectedRouter>
-//       </Provider>
-//     );
+  it('should not crash when not logged in', () => {
+    const falseLogout = (
+      <Provider store={loggedOutStore}>
+        <ConnectedRouter history={history}>
+          <Logout />
+        </ConnectedRouter>
+      </Provider>
+    );
 
-//     const component = mount(falseLogout);
-//     const wrapper = component.find('Button').at(0);
+    const component = mount(falseLogout);
+    const wrapper = component.find('Button').at(0);
 
-//     wrapper.simulate('click');
+    wrapper.simulate('click');
 
-//     expect(spyLogout).toHaveBeenCalledTimes(1);
-//   });
-// });
+    expect(spyLogout).toHaveBeenCalledTimes(1);
+  });
+});
