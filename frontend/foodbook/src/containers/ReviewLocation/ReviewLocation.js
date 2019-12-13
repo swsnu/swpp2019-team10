5b90b63880ca19e4345b3e863d6ffb566aac4be6
laptop <54742258+th-snu@users.noreply.github.com>
2019-12-13 17:57:27 +0900

    Update ReviewLocation.js

--- frontend/foodbook/src/containers/ReviewLocation/ReviewLocation.js
+++ frontend/foodbook/src/containers/ReviewLocation/ReviewLocation.js
@@ -9,6 +9,26 @@ import GoogleMap from 'components/GoogleMap';
 import ReviewPreview from 'components/ReviewPreview/';
 // import * as actionCreators from 'store/actions/review/action_review';
 
+const distance = (lat1, lng1, lat2, lng2) => {
+  if ((lat1 === lat2) && (lng1 === lng2)) {
+    return 0;
+  }
+
+  const radlat1 = (Math.PI * lat1) / 180;
+  const radlat2 = (Math.PI * lat2) / 180;
+  const theta = lng1 - lng2;
+  const radtheta = (Math.PI * theta) / 180;
+  let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  if (dist > 1) {
+    dist = 1;
+  }
+  dist = Math.acos(dist);
+  dist = dist * 180 / Math.PI;
+  dist = dist * 60 * 1.1515;
+  dist *= 1.609344;
+  return dist;
+};
+
 class ReviewLocation extends Component {
   constructor(props) {
     super(props);
